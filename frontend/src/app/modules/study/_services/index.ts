8f57fe924a0e76f621bde6d53a733f95c16f6932
Miguel de la Fuente <mjdelafuente@gmail.com>
2021-11-13 21:25:43 -0300

    .

---
+++ frontend/src/app/modules/study/_services/index.ts
@@ -0,0 +1,4 @@
+// Services
+
+export { StudyService } from './study.service'; // You have to uncomment this, when your real back-end is done
+export { TypeStudyService } from './study.service'; // You have to uncomment this, when your real back-end is done
